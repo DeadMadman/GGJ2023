3c3402f63eaa14070a306eba83570d09ee3387f1
Stick <rutaspk@gmail.com>
2023-02-04 21:40:48 +0100

    some channges

---
+++ Assets/Script/AudioManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioManager manager;
+
+    void PlayClip(AudioClip clip)
+    {
+        
+    }
+
+
+}
